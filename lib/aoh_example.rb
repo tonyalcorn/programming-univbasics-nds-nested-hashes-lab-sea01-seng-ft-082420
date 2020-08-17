51c5c44115829829f458be85cc1d81c9b34c5abe
Anthony Alcorn <xmastony@gmail.com>
2020-08-17 04:01:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  infinite_jest = [
+    [
     DON_G,
     JOELLE_VD,
     PAT_M,
