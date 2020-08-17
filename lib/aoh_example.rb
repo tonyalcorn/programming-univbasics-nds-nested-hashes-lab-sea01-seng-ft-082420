fd54544b7603d64826ddb15cff5c0c465fef4ed9
Anthony Alcorn <xmastony@gmail.com>
2020-08-17 03:25:36 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,7 @@ def assembled_aoh
 end
 
 def literal_aoh 
-  DON_G = [{:name=>"Don Gately, occupation:=>"Live-in Staff"}]
+  DON_G = [{:name=>"Don Gately, :occupation=>"Live-in Staff"}]
   
 end
 
